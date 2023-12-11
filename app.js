8a5f798f8dded8ba080bfc253542d1864884471f
Averruz <franciscoaverruz7@gmail.com>
2023-12-11 22:07:06 +0100

    prueba de conexion, codigo limpio

--- app.js
+++ app.js
@@ -3,64 +3,23 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
-
 require("dotenv").config();
+
+// SERVIDOR ===================================================================================================
 const mongoose = require("mongoose");
-
-const mongoDB =
-  "mongodb+srv://" +
-  process.env.DB_USER +
-  ":" +
-  process.env.DB_PASSWORD +
-  "@" +
-  process.env.DB_SERVER +
-  "/" +
-  process.env.DB_NAME +
-  "?retryWrites=true&w=majority";
-
-console.log(mongoDB, "mongodb");
+const mongoDB ="mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_SERVER + "/" + process.env.DB_NAME + "?retryWrites=true&w=majority";
 async function main() {
   await mongoose.connect(mongoDB);
 }
-
 main().catch((err) => console.log(err));
-var users = require('./routes/user')
-app.use('/users/', users)
+
+var users = require("./routes/user");
+app.use("/users/", users);
 
 app.get("/", (req, res) => {
-  console.log(process.env.DB_USER);
   res.send("Task API backend");
 });
 
 app.listen(port, () => {
-  console.log("server running on port : ", port);
+  console.log(`Example app listening on port ${port}`);
 });
-// =======================
-
-// const express = require('express')
-// const app = express()
-// const port = 8000
-
-// app.use(express.json())
-
-// require('dotenv').config();
-
-// // SERVIDOR ===================================================================================================
-// const mongoose = require("mongoose");
-// const mongoDB = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@"+process.env.DB_SERVER+"/"+process.env.DB_NAME+"?retryWrites=true&w=majority";
-// async function main() {
-//   await mongoose.connect(mongoDB);
-// }
-// main().catch(err => console.log(err));
-
-// var users = require('./routes/user')
-// app.use('/users/', users)
-
-// app.get('/', (req, res) => {
-//   // console.log(process.env.DB_USER)
-//   res.send('Task API backend')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
--- models/user.model.js
+++ models/user.model.js
@@ -1,31 +1,10 @@
-// const mongoose = require('mongoose');
-
-// const Schema = mongoose.Schema;
-
-// var userSchema = new Schema({
-
-//     nombre: String,
-//     biografia: String,
-//     // fecha_de_nacimiento: Date,
-//     nacionalidad: String
-
-// }); 
-
-// module.exports = mongoose.model('user', userSchema);
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-// const status = {
-//   PENDING: 'PENDING',
-//   IN_PROGRESS: 'IN PROGRESS',
-//   COMPLETED: 'COMPLETED'
-// };
 
 const userSchema = new Schema({
   nombre: String,
   apellido: String,
-  nacionalidad: String,
+  nacionalidad: String
 });
 
 module.exports = mongoose.model("User", userSchema);
