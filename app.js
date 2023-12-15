637ab82ff380601e022cc67181f0d2490496cdc0
Jesus Carrasco <sau8719@gmail.com>
2023-12-15 19:08:28 +0100

    dar formato

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ require("dotenv").config();
 
 // SERVIDOR ===================================================================================================
 const mongoose = require("mongoose");
-const mongoDB ="mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_SERVER + "/" + process.env.DB_NAME + "?retryWrites=true&w=majority";
+const mongoDB =
+  "mongodb+srv://" +
+  process.env.DB_USER +
+  ":" +
+  process.env.DB_PASSWORD +
+  "@" +
+  process.env.DB_SERVER +
+  "/" +
+  process.env.DB_NAME +
+  "?retryWrites=true&w=majority";
 async function main() {
   await mongoose.connect(mongoDB);
 }
