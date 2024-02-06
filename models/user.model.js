6da99be5c4fdfa02388ca173dc05ff1af4eaf0ef
Averruz <franciscoaverruz7@gmail.com>
2024-02-06 19:43:41 +0100

    actualizacion jwt

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const { stringify } = require("querystring");
 const Schema = mongoose.Schema;
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
+const jwt = require ("jsonwebtoken");
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
+
+const UserSchema = new Schema({
+  nameu: { type: String, required: true },
   lastName: { type: String, required: true },
   phone: { type: String, required: true },
   email: { type: String, required: true, unique: true },
@@ -20,15 +22,47 @@ const userSchema = new Schema({
   timestamps:true
 });
 
-// userSchema.statics.encryptPassword = async (password) => {
-//   const salt = await bcrypt.genSalt(10)
-//   return await bcrypt.hash(password, salt)
-// }
+//metodo statico de signup
 
-// userSchema.statics.comparePassword = async (password, receivedPassword) => {
-//   return await bcrypt.compare (password, receivedPassword)
-// }
+UserSchema.statics.signup = async function (nameu, lastName, phone, email, DoB, country, docType, docNum, username, isAdmin, pwd) {
+
+  const exists = await this.findOne({email})
+
+  if (exists) {
+    throw Error ('El email ya está registrado')
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(pwd, salt)
+
+  const user = await this.create({nameu, lastName, phone, email, DoB, country, docType, docNum, username, isAdmin, pwd:hash})
+
+  return user
+
+}
+
+UserSchema.methods.comparePassword = function (pwd) {
+  
+  // console.log('pwd', pwd)
+  return bcrypt.compareSync(pwd, this.pwd);
+};
+
+UserSchema.methods.generateJWT = function () {
+  console.log('this', this)
+  const today = new Date();
+  const expirationDate = new Date();
+
+  expirationDate.setDate(today.getDate() + 60);
+
+  let payload = {
+    id: this._id,
+  };
+  // method from the json-web-token library (who is in charge to generate the JWT)
+  console.log('secret', process.env.JWT_SECRET)
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: parseInt(expirationDate.getTime() / 1000, 10),
+  });
+};
 
 
-
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../controllers/userController.js')
+const { loginUser, signupUser } = require('../controllers/authController.js')
 
 router.post('/', userController.createUser )
 router.get('/', userController.retUsersAll )
 router.get('/:id', userController.retUserById )
 router.put('/:id', userController.updateUser )
 router.delete("/:id", userController.deleteUser)
+
+// login route
+router.post('/login', loginUser)
+
+//signup route
+router.post('/signup', signupUser)
 
 module.exports = router
