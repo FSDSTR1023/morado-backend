0058cc0eedf4cae6c46c1132f0c4a433a29cacad
Jesus Carrasco <sau8719@gmail.com>
2024-02-06 20:16:30 +0100

    eliminar auth

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const auth = require('../controllers/authController.js')
-
-router.post("/", auth.register)
-router.post("/", auth.login)
-
-module.exports = router
