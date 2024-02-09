80e3da93fc3ab22877da7f4303a9147054eec794
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-02-09 12:02:20 +0100

    Se cambio el puerto por el 6000

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 5000;
-const cors = require ('cors')
+const port = 6000;
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 
-require("dotenv").config();
+require('dotenv').config();
 
 // SERVIDOR ===================================================================================================
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const mongoDB =
-  "mongodb+srv://" +
+  'mongodb+srv://' +
   process.env.DB_USER +
-  ":" +
+  ':' +
   process.env.DB_PASSWORD +
-  "@" +
+  '@' +
   process.env.DB_SERVER +
-  "/" +
+  '/' +
   process.env.DB_NAME +
-  "?retryWrites=true&w=majority";
+  '?retryWrites=true&w=majority';
 async function main() {
   await mongoose.connect(mongoDB);
 }
 main().catch((err) => console.log(err));
 
-var users = require("./routes/user");
-var room = require("./routes/room");
-var bookings = require("./routes/bookings");
+var users = require('./routes/user');
+var room = require('./routes/room');
+var bookings = require('./routes/bookings');
 //var auth = require("./routes/auth");
 
-app.use("/users", users);
-app.use("/rooms", room);
-app.use("/bookings", bookings);
+app.use('/users', users);
+app.use('/rooms', room);
+app.use('/bookings', bookings);
 // app.use("/register", auth);
 
-app.get("/", (req, res) => {
-  res.send("Creamos usuarios");
-}); 
+app.get('/', (req, res) => {
+  res.send('Creamos usuarios');
+});
 
 app.listen(port, () => {
   console.log(`Aplicación escuchando en puerto: ${port}`);
